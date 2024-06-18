d64da55607075bfd2299854a90129db1a324e3e8
joao pedro <joao.pedro73210@gmail.com>
2024-06-18 09:07:04 -0300

    Initial Commit

---
+++ PetErcise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetErcise
+//
+//  Created by João Pedro Albuquerque on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetErcise/PetErciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetErciseApp.swift
+//  PetErcise
+//
+//  Created by João Pedro Albuquerque on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PetErciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
